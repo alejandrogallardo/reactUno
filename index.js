9d6b7583f966b7335dd32749204b59dd85cb299b
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2018-07-20 08:58:07 -0600

    agregando playlist

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Media from './src/playlist/components/media';
+import Playlist from './src/playlist/components/playlist';
+import data from './src/api.json';
 
 const app = document.getElementById('app')
 // console.log('Prueba de React')
 // const hoaMundo = <h1>React con Webpack!</h1>;
-render(<Media title="Desarrollo Web" author="Alejandro Gallardo" image="./images/covers/html5.jpg"/> , app);
+render(<Playlist data={data} /> , app);
