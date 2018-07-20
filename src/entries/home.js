bce0a8549ba18f6a8809a6d5f446db3b396f090d
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2018-07-20 10:59:00 -0600

    Estructuracion de containers y UI Components: Categorias

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Playlist from './src/playlist/components/playlist';
-import data from './src/api.json';
+import Home from '../pages/containers/home';
+// import Playlist from './src/playlist/components/playlist';
+import data from '../api.json';
 
 const app = document.getElementById('app')
 // console.log('Prueba de React')
 // const hoaMundo = <h1>React con Webpack!</h1>;
-render(<Playlist data={data} /> , app);
+render(<Home data={data} /> , app);
